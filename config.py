c56bb2b5524fd9bc65a4bf360340a9d4396e5b75
vlad-shev <vlad-shev@ukr.net>
2017-04-02 04:05:25 +0300

    token deleted

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 TOKEN = "token"
 CLIENT_SECRETS_FILE = 'client_secret.json'
-SCOPE = 'https://www.googleapis.com/auth/drive'
 REDIRECT_URI = 'http://127.0.0.1:5000/oauth2callback'
-SERVICE_NAME = 'drive'
 API_VERSION = 'v3'

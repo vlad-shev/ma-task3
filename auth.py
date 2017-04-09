10e9a2106ecdb692e99d69e80533b1597e993f76
vlad-shev <vlad-shev@ukr.net>
2017-04-10 00:15:59 +0300

    auth update

--- auth.py
+++ auth.py
@@ -6,24 +6,11 @@ from googleapiclient.discovery import build
 
 from config import TOKEN, CLIENT_SECRETS_FILE, REDIRECT_URI, API_VERSION
 from flask import Flask, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-# TODO
-# 1.Get user_id from Telegram
-# 2 If token for user_id in db get token from db
-#  3.1 Else make request to google and get token for user_id
-#  3.2 Save token in db for user_id
-
-engine = create_engine('sqlite:///users.db')
-Base = declarative_base()
-Base.metadata.create_all(engine)
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
+from models.user import User
 app = Flask(__name__)
 bot = telebot.TeleBot(TOKEN)
+
+user_dict = {}
 scope = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/calendar'
 # OAuth scope that need to request to access Google APIs
 flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
@@ -31,17 +18,34 @@ flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
                                redirect_uri=REDIRECT_URI)  # Make app client object.
 # Application uses the client object to perform OAuth 2.0 operations, such as generating authorization request URLs
 #  and applying access tokens to HTTP requests.
-auth_url = flow.step1_get_authorize_url()  # Get url to Google authorization server
 
 
 @bot.message_handler(commands=['login'])  # Login in GoogleDrive
-def drive_auth(message):
+def request_contact(message):
+    msg = bot.send_message(message.chat.id, text='Please send your phone')
+    bot.register_next_step_handler(msg, get_contact)
+
+
+def get_contact(message):
+    user_dict['user_id'] = int(message.from_user.id)
+    user_dict['username'] = message.from_user.first_name
+    user_dict['phone'] = int(message.text)
+    msg = bot.send_message(message.chat.id, text='Please send your email')
+    bot.register_next_step_handler(msg, get_email)
+
+
+def get_email(message):
+    user_dict['email'] = message.text
+    bot.send_message(message.chat.id, 'Hi, {} {}'.format(user_dict['username'], user_dict['email']))
+
+    bot.send_message(message.chat.id, text='Please sign in to Google account')
+    auth_url = flow.step1_get_authorize_url()  # Get url to Google authorization server
     keyboard = telebot.types.InlineKeyboardMarkup()
-    url_button = telebot.types.InlineKeyboardButton(text='Authenticate',
+    url_button = telebot.types.InlineKeyboardButton(text='Google authentication',
                                                     url=auth_url)
 
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, 'Google Drive&Calendar', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Drive + Calendar', reply_markup=keyboard)
     # Make url button to Google authorization server
 
 
@@ -49,8 +53,20 @@ def drive_auth(message):
 def get_credentials():                            # app get code which will exchange for access token.
     credentials = flow.step2_exchange(request.args.get('code'))  # Exchange authorization code for access token
     # Create json credentials object using Credentials.to_json(credentials)
-    # Add credentials to database
+    json_credentials = Credentials.to_json(credentials)
+    user_dict['google_token'] = json_credentials
+    user = User(user_id=user_dict['user_id'], username=user_dict['username'], email=user_dict['email'],
+                phone=user_dict['phone'], google_token=user_dict['google_token'])
+    #session.add(user)
+    #sesion.commit()
+    # Add user to database
     return '200'
+
+
+def build_service(credentials, service_name):
+    http = credentials.authorize(httplib2.Http())  # Apply the access token to an Http object
+    service = build(service_name, API_VERSION, http=http)  # Build a service object
+    return service
 
 """
 @app.route('/test/drive')
@@ -59,13 +75,6 @@ def test_drive():
     files = drive.files().list().execute()  # List all files in GDrive
     return json.dumps(files)
 """
-
-
-def build_service(credentials, service_name):
-    http = credentials.authorize(httplib2.Http())  # Apply the access token to an Http object
-    service = build(service_name, API_VERSION, http=http)  # Build a service object
-    return service
-
 
 if __name__ == '__main__':
     bot.polling()  # kill bot.polling() (Ctrl + C) after getting link in Telegram to start app.run()
